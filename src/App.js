7ae6dcb05261902d2cd5830682da0d3262e471c4
adekunle <adekunle@deimos.co.za>
2020-06-15 21:08:10 +0100

    add about page

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import './App.css';
 import { Card } from '@shopify/polaris';
 import SignIn from './pages/SignIn';
 import Dashboard from './pages/Dashboard';
+import About from './pages/About';
 
 function App() {
   return (
     <div className='App'>
       <Card title='welcome' sectioned>
         <nav>
-          <Link to='/'>dashboard</Link> | <Link to='/login'>login</Link>
+          <Link to='/'>dashboard</Link> | <Link to='/login'>login</Link> |{' '}
+          <Link to='/about'>about</Link>
         </nav>
         <Router>
           <SignIn path='login' />
           <Dashboard path='/' />
+          <About path='about' />
         </Router>
       </Card>
     </div>
